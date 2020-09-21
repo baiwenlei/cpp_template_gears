4b1e73befc7683fe2289c6a7f509f1c5b7485d30
baiwenlei <baiwenlei@gmail.com>
2020-09-21 11:20:27 +0800

    Update index_sequence.hpp

--- include/cpp_template_gears/index_sequence.hpp
+++ include/cpp_template_gears/index_sequence.hpp
@@ -3,19 +3,19 @@
 namespace cpp_template_gears {
 
 template <size_t... I>
-struct my_index_sequence {};
+struct index_sequence {};
 
 namespace detail {
 template <size_t N, size_t... Nexts>
-struct make_my_index_sequence : public make_my_index_sequence<N-1U, N-1U, Nexts...> {};
+struct make_index_sequence : public make_index_sequence<N-1U, N-1U, Nexts...> {};
 
 template <size_t... Nexts>
-struct make_my_index_sequence<0u, Nexts...> {
-    using type = my_index_sequence<Nexts...>;
+struct make_index_sequence<0U, Nexts...> {
+    using type = index_sequence<Nexts...>;
 };
 } // namespace detail
 
 template <size_t N>
-using make_my_index_sequence = typename detail::make_my_index_sequence<N>::type;
+using make_index_sequence = typename detail::make_index_sequence<N>::type;
 
 }
