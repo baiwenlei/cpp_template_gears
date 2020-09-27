394f3295a0620c2cf6dbc5db852d8f359848e72f
baiwenlei <baiwenlei@gmail.com>
2020-09-27 14:58:40 +0800

    Create scheduler.hpp

---
+++ include/cpp_template_gears/actor/scheduler.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <memory>
+
+namespace actor {
+
+class Mailbox;
+
+class Scheduler {
+public:
+    virtual ~Scheduler() = default;
+    virtual void scheduleMailbox(std::weak_ptr<Mailbox> mailbox) = 0; 
+};
+
+}
